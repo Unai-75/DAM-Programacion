731cb59d8fd886ab851c78e8769a351a8d90bad4
Unai-75 <unaidelacruz156@gmail.com>
2025-09-17 12:37:53 +0200

    Add files via upload
    Programa que calcula el área de un círculo

---
+++ AreaCirculo.java
@@ -0,0 +1,16 @@
+//Crea un programa que calcule el área de un circulo
+//dado su radio
+import java.util.Scanner;
+public class AreaCirculo {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner (System.in);
+
+
+		System.out.print("Introduce el radio del círculo: ");
+		double radio=scanner.nextDouble();
+
+
+		double area=Math.PI * Math.pow(radio, 2);
+		System.out.println("El área del círculo es: " + area);
+	}
+}
